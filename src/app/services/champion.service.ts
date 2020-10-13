f57b55276dffaedb806caa523fef2a4cd3d2c38d
Azorimor <azorimor@gmail.com>
2020-10-13 11:44:35 +0200

    Created Service for random Champion

--- src/app/services/champion.service.ts
+++ src/app/services/champion.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Champion } from '../data/champion';
 
@@ -9,6 +9,7 @@ import { Champion } from '../data/champion';
 })
 export class ChampionService {
 
+  // TODO put these values in environment file
   private PATCH_VERSION = '10.20.1';
   private COMMUNITY_API_SERVER = 'https://cdn.communitydragon.org/latest';
   private DDRAGON_API_SERVER = 'http://ddragon.leagueoflegends.com/cdn/' + this.PATCH_VERSION;
@@ -19,18 +20,26 @@ export class ChampionService {
    * Get a random Champion.
    */
   public getRandomChampion(): Observable<any> { // TODO champion type
-    return this.http.get(this.DDRAGON_API_SERVER + '/data/en_US/champion.json');
-  }
-
-  /**
-   * Get all available Champions
-   */
-  private getChampions(): Observable<Champion[]> {
     return this.http
       .get<any>(this.DDRAGON_API_SERVER + '/data/en_US/champion.json')
       .pipe(
         map((res: any) => {
-          return res.response;
+          const champs: Champion[] = [];
+          for (const key in res.data) {
+            if (res.data.hasOwnProperty(key)){
+              const newChamp: Champion = {
+                id: res.data[key].id,
+                key: res.data[key].key,
+                name: res.data[key].name,
+                avatar: `http://ddragon.leagueoflegends.com/cdn/${this.PATCH_VERSION}/img/champion/${key}.png`,
+                title: res.data[key].title,
+                blurb: res.data[key].blurb,
+              };
+              champs.push(newChamp);
+            }
+          }
+          const randomChampion: Champion = champs[Math.floor(Math.random() * champs.length)];
+          return randomChampion;
         })
       );
   }
