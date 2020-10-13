57b3def5ecb4be9e70b68ad9e08417b58c55ce82
Azorimor <azorimor@gmail.com>
2020-10-13 19:03:13 +0200

    Added Meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
-  title = 'lol-dice-queue';
+export class AppComponent implements OnInit{
+  title = 'LoL - Dice Queue';
+
+  constructor(private titleService: Title, private metaService: Meta) {}
+
+  ngOnInit() {
+    this.titleService.setTitle(this.title);
+    this.metaService.addTags([
+      {name: 'keywords', content: 'League of Legends, dice queue, random champion, würfelqueue'},
+      {name: 'description', content: 'Leage of Legends Dice Queue generates random champions for you.'},
+      {name: 'og:title', content: this.title},
+      {name: 'og:type', content: 'website'}, // TODO change
+      {name: 'og:image', conent: ''}, // TODO add
+      {name: 'og:site_name', conent: 'LoL Dice Queue - Azorimor'},
+      // ...
+    ]);
+  }
 }
